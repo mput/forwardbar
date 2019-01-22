e9d21d75fb3771da61f84c28600cf7374728adb7
mput <putintsev@gmail.com>
2019-01-22 13:46:08 +0300

    Moved blocks to separate directories, add food section

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,35 @@
-// import gulp from 'gulp';
-import connect from 'gulp-connect';
+import fs from 'fs';
+import path from 'path';
 
-export function hello(cb) {
-  console.log('Hello gulp');
-  cb();
+const fsPromises = fs.promises;
+
+export async function organize() {
+  const sourceDir = path.join(__dirname, 'views');
+  const destDir = path.join(sourceDir, 'blocks');
+
+  const scssNames = async (dirName) => {
+    const files = await fsPromises.readdir(dirName);
+    const scss = files
+      .filter(name => path.basename(name) !== 'styles.scss')
+      .filter(name => path.extname(name) === '.scss');
+    return scss;
+  };
+  const getDestDirName = name => path.join(destDir, path.parse(name).name);
+  const getSrcDstNames = name => ({
+    src: path.join(sourceDir, name),
+    dst: path.join(getDestDirName(name), name),
+  });
+
+  const names = await scssNames(sourceDir);
+
+  await Promise.all(names.map(name => fsPromises
+    .mkdir(getDestDirName(name))
+    .catch(e => e)));
+
+  await Promise.all(names
+    .map(name => getSrcDstNames(name))
+    .map(({ src, dst }) => fsPromises
+      .copyFile(src, dst)
+      .then(() => fsPromises.unlink(src))
+      .catch(e => e)));
 }
-
-function defaultTask(cb) {
-  console.log('Form gulp');
-  // place code for your default task here
-  cb();
-}
-
-export default defaultTask;
